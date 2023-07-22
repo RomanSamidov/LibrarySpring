5903200c61ae9865b5fa2fa03494b3cdefa63755
Roman Samidov <roman.samidov@gmail.com>
2023-07-22 22:31:57 +0300

    first controllers and pages

---
+++ src/main/java/my/project/alishevspringcourse/controllers/UsersController.java
@@ -0,0 +1,13 @@
+package my.project.alishevspringcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsersController {
+
+    @GetMapping("/users")
+    public String getBooks(){
+        return "users";
+    }
+}
